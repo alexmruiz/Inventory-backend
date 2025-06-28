b10d07ac6272743062f84b00b40dd6dd703d5579
alexmruiz <alexmyruiz@gmail.com>
2025-06-28 20:06:08 +0200

    eliminar categorias

--- src/main/java/com/company/inventory/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/inventory/services/ICategoryService.java
@@ -11,7 +11,7 @@ public interface ICategoryService {
     public ResponseEntity<CategoryResponseRest> searchById(Long id);
     public ResponseEntity<CategoryResponseRest> save(Category category);
     public ResponseEntity<CategoryResponseRest> update(Category category, Long id);
-
+    public ResponseEntity<CategoryResponseRest> deleteById(Category category, Long id);
 
 
 
